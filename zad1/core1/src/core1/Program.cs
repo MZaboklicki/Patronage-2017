88ae0c100468cafc0c70359e564eed72713a6564
MZaboklicki <michal.dragon@gmail.com>
2016-12-22 20:08:37 +0100

    done

--- zad1/core1/src/core1/Program.cs
+++ zad1/core1/src/core1/Program.cs
@@ -9,40 +9,57 @@ namespace core1
     {
         public static void Main(string[] args)
         {
-            
-                Console.Write("Podaj sciezke katalogu: ");
-                string path = Console.ReadLine();
+
+            Console.Clear();
+            string[] arg = Environment.GetCommandLineArgs();
+            string path;
+            try
+            {
+               path = arg[1];
+            }
+            catch
+            {
+                Console.WriteLine("Nie podano argumentu ");
+                Console.ReadLine();
+                return;
+            }
+           
+                
+                 Console.WriteLine("Podana sciezka: "+path);
                 System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(path);
-                if (dir.Exists)
+            if (dir.Exists)
+            {
+                Console.WriteLine("Katalog istnieje \n");
+                Console.WriteLine("Sciezka do pliku \t \t \t data utworzenia \n");
+                System.IO.FileInfo[] file = dir.GetFiles();
+                int i = 1;
+                if (file.Length == 0)
                 {
-                    Console.WriteLine("Katalog istnieje \n");
-                    Console.WriteLine("Sciezka do pliku \t \t \t data utworzenia \n");
-                    System.IO.FileInfo[] file = dir.GetFiles();
-                    int i = 1;
-                    foreach (System.IO.FileInfo fil in file)
-                    {
+                    Console.WriteLine("Brak plikow do wyswietlenia");
+                }
+                foreach (System.IO.FileInfo fil in file)
+                {
                     string name = fil.FullName;
-                    if(name.Length > 30)
+                    if (name.Length > 30)
                     {
                         name = "...";
-                        for (int j=20; j>0; j--)
+                        for (int j = 20; j > 0; j--)
                         {
                             name = name + fil.FullName[(int)(fil.FullName.Length) - 1 - j];
                         }
                     }
-                        Console.WriteLine(i + " " + " {0} \t \t {1} ",name , fil.LastAccessTime);
-                        i++;
-                    }
+                    Console.WriteLine(i + " " + " {0} \t \t {1} ", name, fil.LastAccessTime);
+                    i++;
                 }
-                else
-                {
-                    Console.WriteLine("Katalog nie istnieje");
-                }
-
-
-                Console.ReadLine();
-
-                 
+                
+            }
+            else
+            {
+                Console.WriteLine("Katalog nie istnieje");
+            }         
+            
+            Console.ReadLine();
         }
+                
     }
 }
