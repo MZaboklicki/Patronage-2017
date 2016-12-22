e4d25e7df5f91b447eaa63bd47cfdb38df37507c
MZaboklicki <michal.dragon@gmail.com>
2016-12-22 17:55:54 +0100

    done

--- zad1/core1/src/core1/Program.cs
+++ zad1/core1/src/core1/Program.cs
@@ -9,32 +9,41 @@ namespace core1
     {
         public static void Main(string[] args)
         {
-            do
-            {
+            
                 Console.Write("Podaj sciezke katalogu: ");
                 string path = Console.ReadLine();
                 System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(path);
                 if (dir.Exists)
                 {
                     Console.WriteLine(dir.FullName);
-                    Console.WriteLine("sciezka istnieje \n");
+                    Console.WriteLine("Katalog istnieje \n");
+                    Console.WriteLine("Sciezka do pliku \t \t \t data utworzenia \n");
                     System.IO.FileInfo[] file = dir.GetFiles();
                     int i = 1;
                     foreach (System.IO.FileInfo fil in file)
                     {
-                        Console.WriteLine(i + " " + String.Format("{0,1} {1,-1} ",fil.FullName , fil.LastAccessTime));
+                    string name = fil.FullName;
+                    if(name.Length > 30)
+                    {
+                        name = "...";
+                        for (int j=20; j>0; j--)
+                        {
+                            name = name + fil.FullName[(int)(fil.FullName.Length) - 1 - j];
+                        }
+                    }
+                        Console.WriteLine(i + " " + " {0} \t \t {1} ",name , fil.LastAccessTime);
                         i++;
                     }
                 }
                 else
                 {
-                    Console.WriteLine("sciezka nie istnieje");
+                    Console.WriteLine("Katalog nie istnieje");
                 }
 
 
                 Console.ReadLine();
 
-            } while (1==1);      
+                 
         }
     }
 }
